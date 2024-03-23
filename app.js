d5044c06017fd8ab0a6cf0a069b8c4de48f17dfa
Ale <alexachavarriaperez@gmail.com>
2024-03-22 18:29:05 -0600

    actualiza nuevas pestanas

--- app.js
+++ app.js
@@ -1,50 +1,16 @@
-/*function cotizar(){
-    window.location.href = 'contacto.html';
-
-}
-var serviciosEscogidos = [];
-
-function agregarServicio(servicio, boton){
-    
-
-    serviciosEscogidos.push(servicio)
-
-    console.log(serviciosEscogidos);
-
-    boton.style.display = 'none';
-
-    boton.nextElementSibling.style.display = 'block';
-
-}
-
-function removerServicio(servicio, boton){
-
-    var indice = serviciosEscogidos.indexOf(servicio);
-    
-    if (indice !== -1) {
-    serviciosEscogidos.splice(indice, 1);
-    }
-
-    console.log(serviciosEscogidos);
-
-
-    boton.style.display = 'none';
-
-    boton.previousElementSibling.style.display = 'block';
-
-    
-}*/
-
-
-
-
-
-
 console.log("Ejecutando el sistema")
 function cotizar(){
-    window.location.href = 'contacto.html';
+    window.location.href = 'servicios.html';
 
 }
+
+console.log("Ejecutando el sistema")
+function ver(){
+    window.location.href = 'galeria.html';
+
+}
+
+/*
 var serviciosEscogidos = [];
 
 
@@ -78,3 +44,4 @@ function removerServicio(servicio, boton){
 
 
 }
+*/
